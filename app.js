64d244cad34daa0f39b37eef1f2f3601f50889eb
RajaMahendra <69353832+maderaja@users.noreply.github.com>
2023-02-02 14:38:47 +0800

    hapus .env hehehe

--- app.js
+++ app.js
@@ -53,4 +53,4 @@ app.post('/tanya', async (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
